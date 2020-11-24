c464331ecf1b073b7840a3523a5a87908ad258be
Guilherme Paiter <guipaiter@gmail.com>
2020-11-24 14:37:35 -0300

    finalizando capitulo 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { environment } from '../environments/environment'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dev Guilhermão';
+  title = 'Compras';
   ambiente = environment.ambiente
 }
