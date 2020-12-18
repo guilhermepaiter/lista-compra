63a053b03a9471789eb366b3ee76139d01fcc95c
Guilherme Paiter <guipaiter@gmail.com>
2020-12-18 08:52:24 -0300

    detales finais

--- src/app/editmercadoria/editmercadoria.component.ts
+++ src/app/editmercadoria/editmercadoria.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MercadoriaService } from '../mercadoria.service';
 import { Mercadoria } from '../mercadoria';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editmercadoria',
@@ -13,6 +14,7 @@ export class EditMercadoriaComponent implements OnInit {
   mercadoria : Mercadoria;
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
     private mercadoriaService: MercadoriaService) { }
 
@@ -25,8 +27,13 @@ export class EditMercadoriaComponent implements OnInit {
     this.mercadoria = this.mercadoriaService.getMercadoria(Number(id));
   }
 
-  salvarMercadoria() {
-    
+  salvarMercadoria() { 
+    this.router.navigateByUrl('/mercadorias');
+  
   }
+
+  salvarMercadoria2 = function () {
+    this.router.navigateByUrl('/mercadorias');
+  };
 
 }
